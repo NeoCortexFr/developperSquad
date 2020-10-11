9e121ec11f416e7d34fd0985451cb19871052a7e
SebastienLatouche <sebastien.latouche.28@gmail.com>
2020-10-11 17:20:14 +0200

    vueJS intégré

---
+++ js/vueJs.js
@@ -0,0 +1,11 @@
+const app = new Vue ({
+    el: "#app",
+    data: {
+        frenchDev: "French.Dev",
+        web: "Web",
+        appli: "Appli Mobile",
+        logiciel: "Logiciel",
+        ia: "Intelligence Artificielle",
+        robot: "Robotique"
+    },
+})
